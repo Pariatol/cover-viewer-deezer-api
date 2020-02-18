87d838c08ee1e60f8828bf80d749c7aba78302d4
Pariatol <pierre.tromeur@gmail.com>
2020-02-18 10:07:20 +0100

    Updates

---
+++ precache-manifest.37583f84766fb0af4c22335239f95275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "855718fe8711d1de4cdbc50f406776a1",
+    "url": "/cover-viewer-deezer-api/index.html"
+  },
+  {
+    "revision": "3eb4bd403521b222e544",
+    "url": "/cover-viewer-deezer-api/static/css/main.00bdd6bf.chunk.css"
+  },
+  {
+    "revision": "11e5901533fda2f4421c",
+    "url": "/cover-viewer-deezer-api/static/js/2.b8571f38.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cover-viewer-deezer-api/static/js/2.b8571f38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eb4bd403521b222e544",
+    "url": "/cover-viewer-deezer-api/static/js/main.f5b34cd2.chunk.js"
+  },
+  {
+    "revision": "440d4a963babfddd7250",
+    "url": "/cover-viewer-deezer-api/static/js/runtime-main.f85e0600.js"
+  }
+]);
